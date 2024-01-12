c4da4417c3755c14812f08c2d9adf45ad11c97c9
DuyTr <duytran3901@gmail.com>
2024-01-13 02:59:22 +0700

    dark mode, light mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,26 @@ import Button from '@mui/material/Button'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ModeToggle></ModeToggle>
       <div>Hello</div>
 
       <Typography variant="body2" color="text.secondary">Test typography</Typography>

22b6de24c850453c10fc8ff93e641279fbd93134
DuyTr <duytran3901@gmail.com>
2024-01-17 23:09:00 +0700

    Code Board Bar

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,15 +1,113 @@
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import Tooltip from '@mui/material/Tooltip'
+import Button from '@mui/material/Button'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+
+const MENU_STYLES = {
+  color: 'primary.main',
+  bgcolor: 'white',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
 
 function boardBar() {
   return (
     <Box sx={{
-      backgroundColor: 'primary.dark',
       width: '100%',
       height: (theme) => theme.trello.boardBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      paddingX: 2,
+      gap: 2,
+      overflowX: 'auto',
+      borderTop: '1px solid #00bfa5'
     }}>
-      Board Bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          sx={MENU_STYLES}
+          icon={<DashboardIcon />}
+          label="DuyTran"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<VpnLockIcon />}
+          label="Public/Private Workspace"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />}
+          label="Add To Google Drive"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<FilterListIcon />}
+          label="Filters"
+          clickable
+        />
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2, color: 'primary.main' }}>
+        <Button variant="outlined" startIcon={<PersonAddIcon />}>Invite</Button>
+        <AvatarGroup
+          max={5}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: '16px'
+            }
+          }}
+        >
+          <Tooltip title='DuyTran'>
+            <Avatar alt="DuyTran" src="https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg" />
+          </Tooltip>
+          <Tooltip title='BunTrinh'>
+            <Avatar alt="DuyTran" src="https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+          </Tooltip>
+          <Tooltip title='HuongTrinh'>
+            <Avatar alt="DuyTran" src="https://images.pexels.com/photos/325044/pexels-photo-325044.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+          </Tooltip>
+          <Tooltip title='Songohan'>
+            <Avatar alt="DuyTran" src="https://images.pexels.com/photos/691668/pexels-photo-691668.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+          </Tooltip>
+          <Tooltip title='KatoriItto'>
+            <Avatar alt="DuyTran" src="https://images.pexels.com/photos/1366957/pexels-photo-1366957.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+          </Tooltip>
+          <Tooltip title='YuraKazuma'>
+            <Avatar alt="DuyTran" src="https://images.pexels.com/photos/1379640/pexels-photo-1379640.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+          </Tooltip>
+          <Tooltip title='DuyTran'>
+            <Avatar alt="DuyTran" src="https://images.pexels.com/photos/1183099/pexels-photo-1183099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+          </Tooltip>
+          <Tooltip title='DuyTran'>
+            <Avatar alt="DuyTran" src="https://images.pexels.com/photos/1485894/pexels-photo-1485894.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
